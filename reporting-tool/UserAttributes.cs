1d257a81eb9c57b7acd160288688e68f954429f2
Philipp Grigoryev <philipp.grigoryev@icsynergy.com>
2019-08-16 17:02:54 -0500

    added "created" to UserAttributes

--- reporting-tool/UserAttributes.cs
+++ reporting-tool/UserAttributes.cs
@@ -1,7 +1,40 @@
+using System.Collections.Generic;
+using Okta.Sdk;
+
 namespace reporting_tool
 {
-    public enum UserAttributes
+    /// <summary>
+    /// Class to define Okta user attributes and operations on them
+    /// </summary>
+    public static class UserAttributes
     {
-        Status, Id
+        /// <summary>
+        /// Collection of non-profile attributes available for reporting and operations
+        /// </summary>
+        public static ICollection<string> NonProfileAttribute => new List<string> {"status", "id", "created"};
+
+    }
+
+    /// <summary>
+    /// Class to extend Okta User object with custom methods
+    /// </summary>
+    public static class OktaUserExtension
+    {
+        /// <summary>
+        /// Method to get access to non-profile fields of User object by passing in their names
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="attrName"></param>
+        /// <returns></returns>
+        public static string GetNonProfileAttribute(this IUser user, string attrName)
+        {
+            switch (attrName)
+            {
+                case "status" : return user.Status;
+                case "id" : return user.Id;
+                case "created" : return $"{user.Created:yyyy/MM/dd}";
+                default : return "";
+            }
+        }
     }
 }
