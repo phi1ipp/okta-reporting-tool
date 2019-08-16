7918542d6356646395ff14dc422bb087d68f9f1a
Philipp Grigoryev <philipp.grigoryev@icsynergy.com>
2019-08-16 10:30:09 -0500

    output formatting changed for Creator report

--- reporting-tool/CreatorReport.cs
+++ reporting-tool/CreatorReport.cs
@@ -1,53 +1,44 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Okta.Sdk;
-using Okta.Sdk.Configuration;
 
 namespace reporting_tool
 {
-    public class CreatorReport
+    /// <inheritdoc />
+    /// <summary>
+    /// Class to execute report to find creators of user entries
+    /// </summary>
+    public class CreatorReport : OktaAction
     {
-        private OktaConfig _oktaConfig;
-        private FileInfo _fileInfo;
+        private readonly FileInfo _fileInfo;
 
-        public CreatorReport(OktaConfig config, FileInfo fileInfo)
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="config">Okta Configuration class</param>
+        /// <param name="fileInfo">File path to use as a source of user entries to run the report for</param>
+        public CreatorReport(OktaConfig config, FileInfo fileInfo) : base(config)
         {
-            _oktaConfig = config;
             _fileInfo = fileInfo;
         }
 
+        /// <summary>
+        /// Main method to execute report
+        /// </summary>
         public void Run()
         {
-            var oktaClient = new OktaClient(new OktaClientConfiguration
-            {
-                OktaDomain = _oktaConfig.Domain,
-                Token = _oktaConfig.ApiKey
-            });
+            File.ReadLines(_fileInfo.FullName)
+                .Select(line => line.Trim().Split(' ', ',').First())
+                .AsParallel()
+                .ForAll(userId =>
+                    Console.WriteLine(userId + " " + OktaClient.Logs
+                        .GetLogs(
+                            filter: $"eventType eq \"user.lifecycle.create\" and target.id eq \"{userId}\"",
+                            since: DateTime.Now.Add(TimeSpan.FromDays(-180d)).ToString("yyyy-MM-dd"))
+                        .Select(ev => ev.Actor.AlternateId + " " + ev.Actor.Id)
+                        .FirstOrDefault().Result)
+                );
 
-            var enumerator = File.ReadLines(_fileInfo.FullName).Select(line => line.Trim().Split(' ').First()).GetEnumerator();
-            for (int i = 1; enumerator.MoveNext(); i++)
-            {
-                var uid = enumerator.Current;
-                
-                var res = oktaClient.Logs.GetLogs(
-                        filter: $"eventType eq \"user.lifecycle.create\" and target.id eq \"{uid}\"",
-                        since: DateTime.Now.Add(TimeSpan.FromDays(-180d)).ToString("yyyy-MM-dd"))
-                    .Select(ev => ev.Target.First().Id + " " + ev.Actor.AlternateId + " " + ev.Actor.Id)
-                    .First().Result;
-
-                Console.WriteLine(res);
-
-
-                if (i % 40 != 0) continue;
-                
-                Console.WriteLine("Pausing...");
-                Thread.Sleep(80 * 1000);
-            }
-            enumerator.Dispose();
         }
     }
 }
