63a73d1bc4242ff271f4369c7554b8953a0d7f0e
Philipp Grigoryev <philipp.grigoryev@icsynergy.com>
2019-12-26 18:48:12 -0600

    refactored appUser report for unified async processing

--- reporting-tool/AppUserReport.cs
+++ reporting-tool/AppUserReport.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 using Okta.Sdk;
 
@@ -39,64 +37,56 @@ namespace reporting_tool
         /// <returns></returns>
         public override async Task Run()
         {
-            var appId = OktaClient.Applications
+            var appId = await OktaClient.Applications
                 .ListApplications(q: _appLabel)
                 .Select(app => app.Id)
-                .First().Result;
+                .First();
 
             if (appId == null)
             {
                 throw new Exception($"Application {_appLabel} doesn't exist");
             }
 
-            var channel = Channel.CreateUnbounded<Tuple<string, string>>();
-
-            var readers = Task.Run(() => { StartReaders(channel.Reader); });
-
             var lines = _input == null
                 ? Program.ReadConsoleLines()
                 : File.ReadLines(_input.FullName);
 
-            lines
+            var semaphor = new SemaphoreSlim(8);
+
+            var tasks = lines
                 .AsParallel()
-                .ForAll(line =>
-                    channel.Writer.TryWrite(
-                        new Tuple<string, string>(appId, line.Split(new[] {' ', ','}, 1)[0])));
-
-            channel.Writer.Complete();
-            await readers;
-        }
-
-        private void StartReaders(object channelReader)
-        {
-            if (!(channelReader is ChannelReader<Tuple<string, string>> reader))
-            {
-                throw new Exception("Reader is null");
-            }
-
-            var readers = Enumerable.Range(1, 4)
-                .Select(async j =>
+                .Select(async line =>
                 {
-                    while (await reader.WaitToReadAsync())
-                    {
-                        var (appId, userId) = await reader.ReadAsync();
+                    var userId = line.Split(new[] {' ', ','}, 1)[0];
 
-                        try
-                        {
-                            var appUser = await OktaClient.Applications
-                                .GetApplicationUserAsync(appId, userId);
-                            
-                            Console.WriteLine($"{appUser.Id}{_ofs}{appUser.ExternalId}");
-                        }
-                        catch (OktaApiException e)
-                        {
-                            if (e.ErrorSummary.StartsWith("Not found"))
-                                Console.WriteLine($"{userId} is not assigned to application");
-                        }
+                    await semaphor.WaitAsync();
+
+                    try
+                    {
+                        var appUser = await OktaClient.Applications
+                            .GetApplicationUserAsync(appId, userId);
+
+                        Console.WriteLine($"{appUser.Id}{_ofs}{appUser.ExternalId}");
+                    }
+                    catch (OktaApiException e)
+                    {
+                        Console.WriteLine(e.Message.Contains("Not found")
+                            ? $"{userId} !!! user not found"
+                            : $"{userId} !!! exception fetching the user: {e.Message}");
+                        return;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"{userId} !!! exception fetching the user: {e}");
+                        return;
+                    }
+                    finally
+                    {
+                        semaphor.Release();
                     }
                 });
 
-            Task.WhenAll(readers).Wait();
+            await Task.WhenAll(tasks);
         }
     }
 }
